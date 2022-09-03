516f89a1638c3cf4545831c431a25cb2646db309
Tegisty Hailay <tigisthay13@gmail.com>
2022-09-03 06:30:11 +0300

    train model for logistic regression

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -25,4 +25,35 @@ class LRModel:
         self.y_test = y_test
         
         self.clf = LogisticRegression()
+
+
+    def train_model(self, folds=1):
         
+        kf = KFold(n_splits = folds)
+        
+        iterator = kf.split(self.X_train)
+        
+        accuracy_arr = []
+        loss_arr = []
+        for i in range(folds):
+            train_index, valid_index = next(iterator)
+            
+            X_train, y_train = self.X_train.iloc[train_index], self.y_train.iloc[train_index]
+            X_valid, y_valid = self.X_train.iloc[valid_index], self.y_train.iloc[valid_index]
+                        
+            self.clf = self.clf.fit(X_train, y_train)
+            
+            vali_pred = self.clf.predict(X_valid)
+            
+            accuracy = self.calculate_score(y_valid, vali_pred)
+            loss = calculate_loss_function(y_valid, vali_pred)
+            
+            self.__printAccuracy(accuracy, i, label="Validation")
+            self.__printLoss(loss, i, label="Validation")
+            print()
+            
+            accuracy_arr.append(accuracy)
+            loss_arr.append(loss)
+
+            
+        return self.clf, accuracy_arr, loss_arr
